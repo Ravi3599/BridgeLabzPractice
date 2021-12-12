addb1e162493c0e45dce4c5705e061d8d5123b40
Ravi3599 <ravikantdukre2018@gmail.com>
2021-12-12 15:44:25 +0530

    ADDED: Swap.java

---
+++ src/com/bridgelabz/basiccoreprograms/Swap.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.basiccoreprograms;
+
+import java.util.Scanner;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter two numbers");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println("Numbers before swapping are:");
+		System.out.println("a =" + a +"  b ="+ b);
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		System.out.println("Numbers after swapping are:");
+		System.out.println("a ="+ a +"  b ="+ b);
+
+	}
+
+}
