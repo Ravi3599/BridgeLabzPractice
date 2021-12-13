215654bb41415908f2710c323906c8a7900aa550
Ravi3599 <94590162+Ravi3599@users.noreply.github.com>
2021-12-13 18:13:48 +0530

    UPDATED: taken input from command line

--- src/com/bridgelabz/functionalprograms/Distance.java
+++ src/com/bridgelabz/functionalprograms/Distance.java
@@ -1,15 +1,12 @@
 package com.bridgelabz.functionalprograms;
 
-import java.util.Scanner;
-
 public class Distance {
 
 		public static void main(String[] args) {
 
-			Scanner sc = new Scanner(System.in);
 			System.out.println("Enter the co-ordiantes x and y of line");
-			int x = sc.nextInt();
-			int y = sc.nextInt();
+			int x = Integer.valueOf(args[0]);
+			int y = Integer.valueOf(args[1]);
 			double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 			System.out.println("Distance of line from origin is " + distance + " units");
 
