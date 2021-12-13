0e8961c0c188efa4a61fe93dbb8546485657d46a
Ravi3599 <94590162+Ravi3599@users.noreply.github.com>
2021-12-13 18:21:35 +0530

    UPDATED: taken input from command line

--- src/com/bridgelabz/functionalprograms/WindChill.java
+++ src/com/bridgelabz/functionalprograms/WindChill.java
@@ -1,15 +1,12 @@
 package com.bridgelabz.functionalprograms;
 
-import java.util.Scanner;
-
 public class WindChill {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter temparature in Fahrenheit");
-		double t = sc.nextDouble();
-		System.out.println("Enter wind speed in Miles per hour");
-		double v = sc.nextDouble();
+		System.out.println("Above temparature in Fahrenheit");
+		double t = Double.valueOf(args[0]);
+		System.out.println("Above wind speed in Miles per hour");
+		double v = Double.valueOf(args[1]);
 		if(t < 50 && v <= 120 && v >= 3) {
 			double w = 35.74 + 0.6215 * t + (0.4275 * t - 35.75) * Math.pow(v, 0.16);
 			System.out.println("Effective temperature(the wind chill) is "+w);
